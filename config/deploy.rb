59cd32aa98c413421a53bd8d3acce95d665e41e7
gonzalo <gonzalo.invernizz@gmai.com>
2020-09-15 12:23:01 -0300

    agergar tarea al server, consola remota de capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,12 +68,14 @@ end
 namespace :rails do
     desc "Remote console"
     task :console  do
-      run_interactively "bundle exec rails console #{rails_env}"
+      run_interactively "bundle exec rails console #{fetch(:rails_env)
+      }"
     end
   
     desc "Remote dbconsole"
     task :dbconsole do
-      run_interactively "bundle exec rails dbconsole #{rails_env}"
+      run_interactively "bundle exec rails dbconsole #{fetch(:rails_env)
+      }"
     end
 
 
